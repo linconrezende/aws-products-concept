958776594f4019113cc2282b1cc6a0bc76eea5b3
Lincon Rezende <lincon.gomes.cfpt@gmail.com>
2023-01-30 10:14:06 -0300

    first commit

---
+++ FIRST_EXAMPLE_JS/index.mjs
@@ -0,0 +1,92 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand, PutCommand, GetCommand, DeleteCommand } from "@aws-sdk/lib-dynamodb";
+import { v4 as uuidv4 } from 'uuid'
+
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
+const tableName = "products";
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  try {
+    let _routeKey = `${event.httpMethod} ${event.resource}`
+    switch (_routeKey) {
+      case "DELETE /product/{id}":
+        await dynamo.send(
+          new DeleteCommand({
+            TableName: tableName,
+            Key: {
+              id: event.pathParameters.id,
+            },
+          })
+        );
+        body = `Deleted item ${event.pathParameters.id}`;
+        break;
+      case "GET /product/{id}":
+        body = await dynamo.send(
+          new GetCommand({
+            TableName: tableName,
+            Key: {
+              id: event.pathParameters.id,
+            },
+          })
+        );
+        body = body.Item;
+        break;
+      case "GET /products":
+        body = await dynamo.send(
+          new ScanCommand({ TableName: tableName, Limit: 100 })
+        );
+        body = body.Items;
+        break;
+      case "PUT /product":
+        let _reqPut = JSON.parse(event.body);
+        let _itemId = uuidv4()
+        await dynamo.send(
+          new PutCommand({
+            TableName: tableName,
+            Item: {
+              id: _itemId,
+              price: _reqPut.price,
+              name: _reqPut.name,
+            },
+          })
+        );
+        body = `Put item ${_itemId}`;
+        break;
+      case "PATCH /product/{id}":
+        let _reqPatch = JSON.parse(event.body);
+        await dynamo.send(
+          new PutCommand({
+            TableName: tableName,
+            Item: {
+              id: event.pathParameters.id,
+              price: _reqPatch.price,
+              name: _reqPatch.name,
+            },
+          })
+        );
+        body = `Updated item ${event.pathParameters.id}`;
+        break;
+      default:
+        // body =  JSON.stringify(event);
+        throw new Error(`Unsupported route: "${_routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  verbose: true,
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+};
